5dad1a678dfcd652259275599d4c2583f6807b84
Seonghyeon Cho <seonghyeoncho96@gmail.com>
2022-12-12 23:14:00 +0900

    Temp fix for tinacms admin page url

--- .tina/config.ts
+++ .tina/config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID!,   // Get this from tina.io
   token: process.env.TINA_TOKEN!,  // Get this from tina.io
   build: {
-    outputFolder: "admin",
+    outputFolder: "blog/admin",    // XXX: Is this good way?
     publicFolder: "static",
   },
   media: {
